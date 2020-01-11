b2400e728cd97d6d45e84425222a63003369ce87
Albub Yusuf <albubyusuf@gmail.com>
2020-01-11 22:25:45 +0600

    product shows in featured product tabs

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,6 +11,7 @@ class FrontendController extends Controller
 
         $data['title'] = 'Frontend Home';
         $data['latest_products'] = Product::with(['category','brand'])->where('status','active')->orderBy('id','DESC')->limit(6)->get();
+        $data['featured_products'] = Product::with(['category','brand'])->where('status','active')->where('is_featured','1')->orderBy('id','DESC')->limit(6)->get();
        // dd($data);
         return view('front.home',$data);
 
